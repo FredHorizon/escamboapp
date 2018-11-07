f41a8fbd617d6125e743d5b105d57d3c0d0e8799
FredHorizon <fredoliveira.sys@gmail.com>
2018-11-07 20:08:34 +0000

    Configura autenticação para acesso ao 'backoffice'

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::DashboardController < ApplicationController
+  before_action :authenticate_admin! # 'before_action' antes de qualquer ação abaixo.
+
   layout "backoffice" # Busca o backoffice no layout da views
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'home', to: 'home#index'
   end
 
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  # get 'admin', to: 'backoffice/dashboard#index'
+
   namespace :backoffice do
     get 'dashboard', to: 'dashboard#index' # faz com que 'dashboard' aponte para 'dashboard#index'
   end
