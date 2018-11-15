e011ab643b927a8528542b08c0160828c75cc8a8
FredHorizon <fredoliveira.sys@gmail.com>
2018-11-15 03:27:39 +0000

    Cadastra as categorias no Backoffice (parte 4/5)

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -17,16 +19,24 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def edit
-    
+
   end
 
   def update
-    
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "Categoria (#{@category.description}) atualizada com sucesso!"
+    else
+      render :edit
+    end
   end
 
   private
 
-  def params_category
-    (params.require(:category).permit(:description))
-  end
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      (params.require(:category).permit(:description))
+    end
 end
