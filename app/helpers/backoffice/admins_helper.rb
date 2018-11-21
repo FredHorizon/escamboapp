f89c3fb9337f49a3b1f7e84e83b58e2065471518
FredHorizon <fredoliveira.sys@gmail.com>
2018-11-21 05:26:10 +0000

    Refatora as opções de acesso do admin

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,13 +1,9 @@
 module Backoffice::AdminsHelper
-    OptionsForRoles = Struct.new(:id, :description)
-  
-    def options_for_roles
-      options = []
-  
-      Admin.roles_i18n.each do |key, value|
-        options.push(OptionsForRoles.new(key, value))
-      end
-  
-      options
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    Admin.roles_i18n.map do |key, value|
+      OptionsForRoles.new(key, value)
     end
+  end
 end
