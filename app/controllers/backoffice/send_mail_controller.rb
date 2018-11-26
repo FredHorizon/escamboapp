d295cc61e7eb8ea7a854c8b4ac77cdbbf048f075
FredHorizon <fredoliveira.sys@gmail.com>
2018-11-26 04:24:47 +0000

    Implementa modal dinâmico com bootstrap

---
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -0,0 +1,13 @@
+class Backoffice::SendMailController < ApplicationController
+
+    def edit
+        @admin = Admin.find(params[:id])
+         respond_to do |format|
+          format.js
+        end
+      end
+       def create
+        #
+      end
+    
+end
---
+++ test/controllers/backoffice/send_mail_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Backoffice::SendMailControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
