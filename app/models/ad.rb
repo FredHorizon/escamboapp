de49a04ea86be393ec895fdafcfab2ecf6a6e285
FredHorizon <fredoliveira.sys@gmail.com>
2018-11-30 00:07:58 +0000

    Ajusta a aplicação

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,6 +1,9 @@
 class Ad < ActiveRecord::Base
-  belongs_to :category
   belongs_to :member
+  belongs_to :category
+
+  # Scopes
+  scope :last_six, -> { limit(6).order(created_at: :desc) }
 
   # paperclip
   has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
