989f8a0ca0f9837a1fe02623b2fcd34bdd0e316e
FredHorizon <fredoliveira.sys@gmail.com>
2018-12-02 03:33:59 +0000

    Lista os anúncios do membro

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,8 @@
 class Member < ActiveRecord::Base
+  has_many :ads
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
---
+++ test/controllers/site/profile/ads_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Site::Profile::AdsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
